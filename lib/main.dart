97fa55974b9a878adec7ead620398a2ae5615259
LfN4nd0 <luis.ribeiro76@etec.sp.gov.br>
2020-02-27 11:36:24 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Container(
+        color: Colors.cyanAccent,
+        child: Text("hardcore mermao", style: TextStyle(color: Colors.black)),
+      ),
+    ));
+
